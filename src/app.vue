59717c7d21f4c939de3e5976dae0dae763988b0a
vara-prasad-07 <varaprasad2684@gmail.com>
2025-03-29 23:08:17 +0530

    updated code

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+
+  
+  <script>
+  import PlantDiseaseDetection from '/components/PlantDiseaseDetection.vue'
+
+  export default {
+  name: 'App',
+  components: {
+    PlantDiseaseDetection
+  }
+  }
+  </script>
+  <template>
+    <div>
+      
+      <PlantDiseaseDetection />
+    </div>
+  </template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './app.vue'
+
+// Mount Vue application to the #app element in index.html
+createApp(App).mount('#app')
